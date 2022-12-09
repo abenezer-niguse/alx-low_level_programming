09cd42efe6c127f13b09e4c7f454c056fd42944b
abenezer-niguse <abenezerniguse777@gmail.com>
2022-12-09 13:01:24 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * return: return int value 0 (success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a=0; a < 10; a++)
+	{
+		printf("%d", a);
+	}
+	putchar('\n');
+	return (0);
+}
