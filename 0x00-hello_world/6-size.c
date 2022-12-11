baeb06602191c2a2e1825b33721e422ba9f39257
abenezer-niguse <abenezerniguse777@gmail.com>
2022-12-11 12:05:30 -0800

    Size is not grandeur, and territory does not make a nation

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	 printf("Size of char pointer: %d\n", sizeof(char));
+	 printf("\nSize of int pointer: %d\n", sizeof(int));
+	 printf("\nSize of float pointer: %d\n", sizeof(float));
+	 printf("\nSize of long int pointer: %d\n", sizeof(long int));
+	 printf("\nSize of double pointer: %d\n", sizeof(double));
+
+	 return (0);
+}
