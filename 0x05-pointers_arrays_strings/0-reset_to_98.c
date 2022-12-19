710ae542ffd6f0e22e72e85ea666b5be3590a930
abenezer-niguse <abenezerniguse777@gmail.com>
2022-12-18 23:39:30 -0800

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - function that takes a pointer to int as parameters
+ * and update the value
+ * @n: Thise is the entry
+ *
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
