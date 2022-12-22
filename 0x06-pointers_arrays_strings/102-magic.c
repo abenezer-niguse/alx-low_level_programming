d779f36ba08e80e79b8ae2ae6817d97f9defdd4a
abenezer-niguse <abenezerniguse777@gmail.com>
2022-12-22 15:23:44 -0800

    A dream doesn't become reality through magic

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,25 @@
+#include <stdio>
+/**
+ * main - function
+ *Return: 0
+ * /
+
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+	/**
+	 * write code line
+	 * not allowed to use a
+	 * not modify p
+	 * only one ststement
+	 */
+	*(p + 5) = 98;
+	/* ...that prints 98\n */
+	print("a[z] = %d\n", a[2]);
+	return (0);
+}
